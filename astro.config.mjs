01934879c7e1f0ef4b314740394015eb215369bb
Kurt Harriger <kurtharriger@gmail.com>
2026-06-20 14:15:10 -0600

    Remove Astro implementation

--- astro.config.mjs
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'astro/config';
-import react from '@astrojs/react';
-import mdx from '@astrojs/mdx';
-
-export default defineConfig({
-  output: 'static',
-  integrations: [react(), mdx()],
-});
